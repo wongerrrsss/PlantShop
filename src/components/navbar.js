2359d51ee1f9568dde70a2a170023aa77bda0e0e
Tawonga Chilongo <tmchilongo92@gmail.com>
2020-05-25 01:31:47 -0400

    fixed the styles for navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,12 +3,16 @@ import React, { Component } from 'react';
 class Navbar extends Component {
     render() {
         return (
-           <div className="navbar-wrapper">
-               <a href="/">Home</a>
-               <a href="/about">About</a>
-               <a href="/contact">Contact</a>
-               <a href="/faq">FAQ</a>
-           </div>
+            <div class="navbar-wrapper">
+                <nav>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                        <li><a href="/faq">FAQ</a></li>
+                    </ul>
+                </nav>
+            </div>
         )
     }
 }
