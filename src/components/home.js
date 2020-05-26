ce597574b7346a767d23b461c5ec849340e32875
Tawonga Chilongo <tmchilongo92@gmail.com>
2020-05-26 13:08:56 -0400

    added homepage slideshow feature

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import Slider from "./slider";
+
 
 class Home extends Component {
     render() {
         return (
             <div className='homepage-wrapper'>
+                <Slider/>
                 the homepage will go here
             </div>
         )
---
+++ src/components/slider.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Slider from 'react-animated-slider';
+import 'react-animated-slider/build/horizontal.css';
+ 
+class Slider extends Component {
+    render() {
+        return (
+            <div className="slider-wrapper">
+                <Slider/>
+            </div>
+        )
+    }
+}
+
+export default Slider; 
