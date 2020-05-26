6a5490a68b4289e8f3808cb26f1e75d42e6c874d
Tawonga Chilongo <tmchilongo92@gmail.com>
2020-05-26 13:19:30 -0400

    added photos for slideshow

--- src/components/slider.js
+++ src/components/slider.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import Slider from 'react-animated-slider';
-import 'react-animated-slider/build/horizontal.css';
+import ImageGallery from 'react-image-gallery';
+ 
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+];
  
 class Slider extends Component {
-    render() {
-        return (
-            <div className="slider-wrapper">
-                <Slider/>
-            </div>
-        )
-    }
+  render() {
+    return <ImageGallery items={images} />;
+  }
 }
 
 export default Slider; 
