0fc184d0177ef271282d97b426ad695a7ccf436c
Tawonga Chilongo <tmchilongo92@gmail.com>
2020-05-30 15:56:19 -0400

    still working on the about me page, figured out bug in image export

--- src/components/about.js
+++ src/components/about.js
@@ -5,16 +5,17 @@ class About extends Component {
     render() {
         return (
             <div className='about-page'>
-                <div className="floated"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p></div>
-                Corporis cumque perspiciatis quo maxime non laudantium nesciunt id doloribus amet nihil quae quam quisquam 
-                quasi repudiandae consequuntur, voluptas quibusdam nostrum suscipit! Odit architecto commodi, 
-                asperiores impedit aperiam accusantium nesciunt maxime eius quidem. Cupiditate quas quis non, excepturi sequi, 
-                molestias quam, commodi laudantium ullam repellat magnam. Totam nostrum earum vero accusamus ipsum 
-                quisquam temporibus libero unde tenetur quo veritatis similique dolore fugiat debitis fugit blanditiis 
-                dolor id recusandae, quis ea reprehenderit deleniti? 
-                Perferendis molestias amet ea explicabo, exercitationem alias dolores magnam, 
-                neque totam a nobis, 
-                aspernatur dolor voluptates nihil fuga nesciunt labore.
+                <div className="about-page-text-wrapper">
+                    <div className="floated"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p></div>
+                    Corporis cumque perspiciatis quo maxime non laudantium nesciunt id doloribus amet nihil quae quam quisquam 
+                    quasi repudiandae consequuntur, voluptas quibusdam nostrum suscipit! Odit architecto commodi, 
+                    asperiores impedit aperiam accusantium nesciunt maxime eius quidem. Cupiditate quas quis non, excepturi sequi, 
+                    molestias quam, commodi laudantium ullam repellat magnam. Totam nostrum earum vero accusamus ipsum 
+                    quisquam temporibus libero unde tenetur quo veritatis similique dolore fugiat debitis fugit blanditiis 
+                    dolor id recusandae, quis ea reprehenderit deleniti? 
+                    Perferendis molestias amet ea explicabo, exercitationem alias dolores magnam, 
+                    neque totam a nobis, aspernatur dolor voluptates nihil fuga nesciunt labore.
+                </div>
             </div>
         )
     }
