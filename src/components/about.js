8ee89d219139460aaa7f9a8b8661e77729660149
Tawonga Chilongo <tmchilongo92@gmail.com>
2020-06-11 11:49:42 -0400

    finishing up the about component styles

--- src/components/about.js
+++ src/components/about.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Image from '../../static/images/plantshoppicture.jpg';
 
 
 class About extends Component {
@@ -6,16 +7,38 @@ class About extends Component {
         return (
             <div className='about-page'>
                 <div className="about-page-text-wrapper">
-                    <div className="floated"><p>picture here</p></div>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Corporis cumque perspiciatis quo maxime non laudantium nesciunt id doloribus amet nihil quae quam quisquam 
-                    quasi repudiandae consequuntur, voluptas quibusdam nostrum suscipit! Odit architecto commodi, 
-                    asperiores impedit aperiam accusantium nesciunt maxime eius quidem. Cupiditate quas quis non, excepturi sequi, 
-                    molestias quam, commodi laudantium ullam repellat magnam. Totam nostrum earum vero accusamus ipsum 
-                    quisquam temporibus libero unde tenetur quo veritatis similique dolore fugiat debitis fugit blanditiis 
-                    dolor id recusandae, quis ea reprehenderit deleniti? 
-                    Perferendis molestias amet ea explicabo, exercitationem alias dolores magnam, 
-                    neque totam a nobis, aspernatur dolor voluptates nihil fuga nesciunt labore.
+                    <div className="floated">
+                        <p>
+                            <img src={Image} alt='plant shop picture' width="600" height="400"></img>
+                        </p>
+                    </div>
+                    <div className="text">
+                        Lorem ipsum plant dolor sit amet consectetur adipisicing elit.
+                        Corporis cumque perspiciatis quo maxime non laudantium nesciunt id doloribus amet nihil quae quam quisquam 
+                        quasi repudiandae consequuntur, voluptas quibusdam nostrum suscipit! Odit architecto commodi, 
+                        asperiores impedit aperiam accusantium nesciunt maxime eius quidem. Cupiditate quas quis non, excepturi sequi, 
+                        molestias quam, commodi laudantium ullam repellat magnam. Totam nostrum earum vero accusamus ipsum 
+                        quisquam temporibus libero unde tenetur quo veritatis similique dolore fugiat debitis fugit blanditiis 
+                        dolor id recusandae, quis ea reprehenderit deleniti? 
+                        Perferendis molestias amet ea explicabo, exercitationem alias dolores magnam, 
+                        neque totam a nobis, aspernatur dolor voluptates nihil fuga nesciunt labore. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Corporis cumque perspiciatis quo maxime non laudantium nesciunt id doloribus amet nihil quae quam quisquam 
+                        quasi repudiandae consequuntur, voluptas quibusdam nostrum suscipit! Odit architecto commodi, 
+                        asperiores impedit aperiam accusantium nesciunt maxime eius quidem. Cupiditate quas quis non, excepturi sequi, 
+                        molestias quam, commodi laudantium ullam repellat magnam. Totam nostrum earum vero accusamus ipsum 
+                        quisquam temporibus libero unde tenetur quo veritatis similique dolore fugiat debitis fugit blanditiis 
+                        dolor id recusandae, quis ea reprehenderit deleniti? 
+                        Perferendis molestias amet ea explicabo, exercitationem alias dolores magnam, 
+                        neque totam a nobis, aspernatur dolor voluptates nihil fuga nesciunt labore. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Corporis cumque perspiciatis quo maxime non laudantium nesciunt id doloribus amet nihil quae quam quisquam 
+                        quasi repudiandae consequuntur, voluptas quibusdam nostrum suscipit! Odit architecto commodi, 
+                        asperiores impedit aperiam accusantium nesciunt maxime eius quidem. Cupiditate quas quis non, excepturi sequi, 
+                        molestias quam, commodi laudantium ullam repellat magnam. Totam nostrum earum vero accusamus ipsum 
+                        quisquam temporibus libero unde tenetur quo veritatis similique dolore fugiat debitis fugit blanditiis 
+                        dolor id recusandae, quis ea reprehenderit deleniti? 
+                        Perferendis molestias amet ea explicabo, exercitationem alias dolores magnam, 
+                        neque totam a nobis, aspernatur dolor voluptates nihil fuga nesciunt labore.
+                    </div>
                 </div>
             </div>
         )
