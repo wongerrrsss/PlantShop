626ab9dc50d8d59dcb5104b40e787815b7892dec
Tawonga Chilongo <tmchilongo92@gmail.com>
2020-06-12 14:57:35 -0400

    started working on the footer component

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className='footer'>
+
+            </div>
+        )
+    }
+}
+
+export default Footer;
